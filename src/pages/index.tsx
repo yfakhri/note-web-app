05543a6d1e67c48dd8ffbaea786262e6afd8bc9e
Yusuf Fakhri <y.fakhri12@gmail.com>
2023-02-19 18:15:45 +0700

    add google auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
@@ -74,7 +74,9 @@ const AuthShowcase: React.FC = () => {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={
+          sessionData ? () => void signOut() : () => void signIn("google")
+        }
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
